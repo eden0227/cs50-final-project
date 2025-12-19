d6ba43132d551417e1ffb2104268ae5b67f63356
eden0227 <daus0227@gmail.com>
2025-12-19 22:51:24 +0800

    chore(src): add python test program

--- src/main.py
+++ src/main.py
@@ -1,1 +1,32 @@
-print('hello, world')
+import csv
+import sys
+
+
+def main():
+    if len(sys.argv) != 2:
+        print('Usage: python main.py output.csv')
+        sys.exit(1)
+
+    if not sys.argv[1].endswith('.csv'):
+        print('Usage: Data must be a CSV file')
+        sys.exit(2)
+
+    filename = sys.argv[1]
+    with open(filename, 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=["key", "value"])
+        writer.writeheader()
+        for i in range(30, 0, -1):
+            writer.writerow({"key": 'a', "value": i})
+
+    rows = []
+    with open(filename, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            rows.append(row)
+
+    for row in rows:
+        print(f'{row['key']}, {row['value']}')
+
+
+if __name__ == "__main__":
+    main()
